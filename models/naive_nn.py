9073dce58eb5c8326cb1a3b52ffda15eaa3b3c0c
Albert Millert <amillert@roq.ad>
2019-05-29 13:53:34 +0200

    Described layers' weights

--- models/naive_nn.py
+++ models/naive_nn.py
@@ -5,43 +5,42 @@ import torch.nn.functional as F
 class NaiveNN(nn.Module):
     def __init__(self, in_dim, batch_size, f_dim, h_dim):
         super(NaiveNN, self).__init__()
-        self.input_layer = nn.Linear(f_dim, int(h_dim / 2))  # output -> (in_dim x h_dim/2)
-        self.hidden_layer = nn.Linear(int(h_dim / 2), h_dim)  # output -> (in_dim x h_dim)
+        # input         -> (BATCH x f_dim)
+        # input_layer   -> (f_dim x h_dim)
+        self.hidden_layer = nn.Linear(f_dim, h_dim)  
+        # result        -> (BATCH x h_dim)
 
-        #  flatten -> (in_dimm * h_dim)
-        #  output -> (in_dimm * h_dim / 100)
-        self.smaller = nn.Linear(batch_size * h_dim, int(batch_size * h_dim / 100))
-        self.out = nn.Linear(int(batch_size * h_dim / 100), batch_size)
+        # flattened     -> (1 x BATCH*h_dim)
+        # flattened_layer -> (BATCH*h_dim x BATCH*h_dim/100)
+        self.flattened_layer = nn.Linear(batch_size * h_dim, int(batch_size * h_dim / 100))
+        # result        -> (1 x BATCH*h_dim/100)
+        self.output_layer = nn.Linear(int(batch_size * h_dim / 100), batch_size)
+        # result       -> (BATCH)
 
-    def forward(self, inputs):
+    def forward(self, input):
         # (1) input
-        # print()
-        # print()
-        # print()
-        # print(inputs.shape)
+        # print(input.shape)
 
         # (2) 1st hidden layer
         # print(self.input_layer.weight.shape)
-        embed = self.input_layer(inputs)
-        # print(embed.shape)
+        # print(self.hidden_layer.weight.detach().numpy().mean())
+        positive_out = self.hidden_layer(input)
+        # print(positive_out.shape)
 
-        # (3) 2nd hidden layer
-        # print(self.hidden_layer.weight.shape)
-        hidden = self.hidden_layer(embed)
-        # print(hidden.shape)
+        # (3) 2nd hidden layer (flattened)
+        positive_out = positive_out.flatten()
+        # print(positive_out.shape)
+        # print(self.flattened_layer.weight.shape)
+        positive_out = self.flattened_layer(positive_out)
+        # print(positive_out.shape)
 
-        # (4) 3rd hidden layer (flattened)
-        flattened = hidden.flatten()
-        # print(flattened.shape)
-        # print(self.smaller.weight.shape)
-        smaller = self.smaller(flattened)
-        # print(smaller.shape)
-
-        # (5) output layer 
+        # (4) 3rd output layer 
         # print(self.out.weight.shape)
-        output = self.out(smaller)
-        # print(output.shape)
+        positive_out = self.output_layer(positive_out)
+        # print(positive_out.shape)
 
-        # print(F.log_softmax(output).shape)
+        # (5) activation function
+        # print(F.log_softmax(positive_out).shape)
         # exit(11)
-        return F.softmax(output, dim=0)
+        return F.softmax(positive_out, dim=0)
+        return positive_out
